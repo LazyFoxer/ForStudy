8a6671631d748dbb4ee438362dc45a6b60289671
LazyFoxer <o.sorokin98@gmail.com>
2026-01-15 20:51:06 +0300

    Create a game guess the number

---
+++ ConsoleAppTest/ConsoleAppTest/Models/GuesTheNumber.cs
@@ -0,0 +1,67 @@
+﻿
+namespace ConsoleAppTest.Models
+{
+    public class GuesTheNumber
+    {
+        /// <summary>
+        /// Number which we try to guess
+        /// </summary>
+        public int Number { get; set; }
+        /// <summary>
+        /// Max attemp before lose
+        /// </summary>
+        public int MaxAttempt { get; set; }
+
+        private bool _isSecondGamerWin;
+
+        public void Start()
+        {
+            FirstGamerSetTheNumber();
+
+            SecondGamerTryGuessTheNumber();
+
+            ShowResult();
+        }
+
+        private void FirstGamerSetTheNumber()
+        {
+            Number = ReadNumber("Enter the number");
+            MaxAttempt = ReadNumber("Enter max attempt count");
+            Console.Clear();
+        }
+
+        private void SecondGamerTryGuessTheNumber()
+        {
+            var attempt = 0;
+            do
+            {
+                var guess = ReadNumber("Enter your guess");
+                if (guess == Number)
+                {
+                    _isSecondGamerWin = true;   
+                    break;
+                }
+                attempt++;
+            }while(attempt < MaxAttempt);
+        }
+
+        private void ShowResult()
+        {
+            if (_isSecondGamerWin)
+            {
+                Console.WriteLine("Congratz");
+            }
+            else
+            {
+                Console.WriteLine("Looser");
+            }
+        }
+
+        private int ReadNumber(string message) 
+        {
+            Console.WriteLine(message);
+            var numberStr = Console.ReadLine();
+            return int.Parse(numberStr);
+        }
+    }
+}
