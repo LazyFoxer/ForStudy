e042c3e4496c2bbeedad1246bb5240107554fcc9
LazyFoxer <o.sorokin98@gmail.com>
2026-01-29 21:32:09 +0300

    Add indexer for maze

--- ConsoleAppTest/MazeConsole/Models/Maze.cs
+++ ConsoleAppTest/MazeConsole/Models/Maze.cs
@@ -7,12 +7,24 @@ namespace MazeConsole.Models
         public int Width { get; set; }
         public int Height { get; set; }
         public List<BaseCell> Cells { get; set; } = new List<BaseCell>();
-
-        public void ReplaceCell(BaseCell newCell)
+        public BaseCell this[int x, int y]
         {
-            var oldCell = Cells.First(cell => cell.X == newCell.X && cell.Y == newCell.Y);
-            Cells.Remove(oldCell);
-            Cells.Add(newCell);
+            get
+            {
+                return Cells.First(cell => cell.X == x && cell.Y == y);
+            }
+            set
+            {
+                if (!Cells.Any(cell => cell.X == x && cell.Y == y))
+                {
+                    Cells.Add(value);
+                    return;
+                }
+
+                var oldCell = Cells.First(cell => cell.X == value.X && cell.Y == value.Y);
+                Cells.Remove(oldCell);
+                Cells.Add(value);
+            }
         }
     }
 }
