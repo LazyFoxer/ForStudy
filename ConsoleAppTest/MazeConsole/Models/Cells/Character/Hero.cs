6d8b92fa96be8c8e83fc58b88d3b9a3b708b82cb
LazyFoxer <o.sorokin98@gmail.com>
2026-02-03 20:34:13 +0300

    Add GetRandom method and GetNearest method

---
+++ ConsoleAppTest/MazeConsole/Models/Cells/Character/Hero.cs
@@ -0,0 +1,21 @@
+﻿namespace MazeConsole.Models.Cells.Character
+{
+    public class Hero : BaseCell
+    {
+        public Hero(int x, int y) : base(x, y)
+        {
+        }
+
+        public override char Symbol => throw new NotImplementedException();
+
+        public override void InteractWithCell()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool TryStep()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
